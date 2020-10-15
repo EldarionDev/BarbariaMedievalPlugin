226d202a31992da9f0bac623cd300b1235d86173
EldarionDev <janek.schultes.privat@gmail.com>
2020-10-15 15:05:12 +0200

    Finished Contract JSON loading.

--- src/main/java/com/eldarion/barbariaplugin/factions/Contract.java
+++ src/main/java/com/eldarion/barbariaplugin/factions/Contract.java
@@ -2,8 +2,14 @@ package com.eldarion.barbariaplugin.factions;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 class Condition {
@@ -21,11 +27,39 @@ public class Contract {
         GOLD, NAP
     }
 
-    public void load () {
-
+    public Contract(String n) {
+        contractName = n;
     }
 
-    public void save (JSONObject object) {
+    public void load () {
+            JSONParser jsonParser = new JSONParser();
+            try {
+                JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("contracts/" + contractName + ".json"));
+                executeInterval = (double) jsonObject.get("execute_interval");
+                deadLine = (double) jsonObject.get("dead_line");
+                conditionFaction = Factions.getFaction(jsonObject.get("condition_faction").toString());
+                rewardFaction = Factions.getFaction(jsonObject.get("reward_faction").toString());
+                JSONObject condition_section = (JSONObject) jsonObject.get("Conditions");
+                JSONObject reward_section = (JSONObject) jsonObject.get("Rewards");
+                Iterator keysToCopyIterator = condition_section.keySet().iterator();
+                while(keysToCopyIterator.hasNext()) {
+                    String key = (String) keysToCopyIterator.next();
+                    conditions.add(new Condition(Conditions.valueOf(key), (double) condition_section.get(key)));
+                }
+                Iterator keysToCopyIteratorRewards = reward_section.keySet().iterator();
+                while(keysToCopyIteratorRewards.hasNext()) {
+                    String key = (String) keysToCopyIterator.next();
+                    conditions.add(new Condition(Conditions.valueOf(key), (double) condition_section.get(key)));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public void save () {
+        JSONObject object = new JSONObject();
         JSONObject condition_segment = new JSONObject();
         JSONObject reward_segment = new JSONObject();
         for (Condition c : conditions) {
@@ -40,6 +74,14 @@ public class Contract {
         object.put("execute_interval", executeInterval);
         object.put("condition_faction", conditionFaction.getFactionName());
         object.put("reward_faction", rewardFaction.getFactionName());
+        try {
+            FileWriter jsonFile = new FileWriter("contracts/" + contractName + ".json");
+            jsonFile.write(object.toJSONString());
+            jsonFile.close();
+        }
+        catch (IOException e) {
+
+        }
     }
 
     public void addCondition (Conditions condition, double amplifier) {
@@ -52,6 +94,7 @@ public class Contract {
         rewards.add(c);
     }
 
+    String contractName;
     double executeInterval;
     double deadLine;
     Faction conditionFaction;
